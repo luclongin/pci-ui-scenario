47878b28e1743b87be884a9d0ab8bd7ac1732274
Luc <luc.longin@gmail.com>
2023-11-10 07:12:08 -0500

    [Task #3] Changing default date format to US date format (mm/dd/yyyy) using valueFormatter

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,20 +1,32 @@
 import { AgGridReact } from "ag-grid-react";
-import { ColDef } from "ag-grid-community";
+import { ColDef, ValueFormatterParams } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
+function dateFormatter(params: ValueFormatterParams) {
+  // current date format is yyyy-mm-dd
+	const dateToArray = params.value.split("T")[0].split("-");
+  // return US date format (mm/dd/yyyy)
+  const newDate = dateToArray[1] + "/" + dateToArray[2] + "/" + dateToArray[0];
+  return newDate;
+}
+
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+	{ field: "designation", headerName: "Designation" },
+	{
+		field: "discovery_date",
+		headerName: "Discovery Date",
+		valueFormatter: (params) => dateFormatter(params)
+	},
+	{ field: "h_mag", headerName: "H (mag)" },
+	{ field: "moid_au", headerName: "MOID (au)" },
+	{ field: "q_au_1", headerName: "q (au)" },
+	{ field: "q_au_2", headerName: "Q (au)" },
+	{ field: "period_yr", headerName: "Period (yr)" },
+	{ field: "i_deg", headerName: "Inclination (deg)" },
+	{ field: "pha", headerName: "Potentially Hazardous" },
+	{ field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true },
 ];
 
 const NeoGrid = (): JSX.Element => {
